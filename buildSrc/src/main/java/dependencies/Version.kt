0a83ef0b8b65379cb42580255a7a9b9d8eb561e4
Georgy Dzutsev <dzgeorgy@gmail.com>
2021-03-24 21:18:02 +0300

    Update dependencies
    Signed-off-by: Georgy Dzutsev <dzgeorgy@gmail.com>

--- buildSrc/src/main/java/dependencies/Version.kt
+++ buildSrc/src/main/java/dependencies/Version.kt
@@ -11,11 +11,11 @@ object Version {
 
     //Libraries
     const val kotlin = "1.4.31"
-    const val compose = "1.0.0-beta02"
-    const val core = "1.5.0-beta03"
-    const val appcompat = "1.3.0-beta01"
-    const val lifecycle = "2.3.0"
-    const val activity = "1.3.0-alpha04"
+    const val compose = "1.0.0-beta03"
+    const val core = "1.5.0-rc01"
+    const val appcompat = "1.3.0-rc01"
+    const val lifecycle = "2.3.1"
+    const val activity = "1.3.0-alpha05"
 
     //Test libraries
     const val junit = "4.13.2"
