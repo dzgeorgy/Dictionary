ff18fd6444c6b5b8fc5bbae715787cb1e1325dc5
Georgy Dzutsev <dzgeorgy@gmail.com>
2021-03-24 14:48:27 +0300

    Dependencies management
    Signed-off-by: Georgy Dzutsev <dzgeorgy@gmail.com>

---
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -0,0 +1,41 @@
+package dependencies
+
+object Dependencies {
+
+    private const val path = "../commonFiles/gradleScript/"
+    const val common = "${path}common.gradle"
+
+    const val dependency = "./gradleScript/dependencies.gradle"
+
+    object Module
+
+    object Compose {
+        const val ui = "androidx.compose.ui:ui:${Version.compose}"
+        const val material = "androidx.compose.material:material:${Version.compose}"
+        const val tooling = "androidx.compose.ui:ui-tooling:${Version.compose}"
+        const val test = "androidx.compose.ui:ui-test-junit4:${Version.compose}"
+    }
+
+    object Core {
+        const val core = "androidx.core:core-ktx:${Version.core}"
+    }
+
+    object AppCompat {
+        const val appcompat = "androidx.appcompat:appcompat:${Version.appcompat}"
+    }
+
+    object Lifecycle {
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifecycle}"
+        const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifecycle}"
+    }
+
+    object Activity {
+        const val compose = "androidx.activity:activity-compose:${Version.activity}"
+    }
+
+    object Test {
+        const val junit = "junit:junit:${Version.junit}"
+        const val androidTest = "androidx.test.ext:junit:${Version.androidTest}"
+    }
+
+}
---
+++ buildSrc/src/main/java/dependencies/Version.kt
@@ -0,0 +1,23 @@
+package dependencies
+
+object Version {
+    //Android
+    const val buildTools = "30.0.3"
+    const val compileSdk = 30
+    const val targetSdk = 30
+    const val minSdk = 21
+    const val versionCode = 1
+    const val versionName = "0.1.0"
+
+    //Libraries
+    const val kotlin = "1.4.31"
+    const val compose = "1.0.0-beta02"
+    const val core = "1.5.0-beta03"
+    const val appcompat = "1.3.0-beta01"
+    const val lifecycle = "2.3.0"
+    const val activity = "1.3.0-alpha04"
+
+    //Test libraries
+    const val junit = "4.13.2"
+    const val androidTest = "1.1.2"
+}
